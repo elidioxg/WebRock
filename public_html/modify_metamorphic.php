fcca57ed70adfda79248d7a37e463670b61e1284
elidioxg <elidioxg@gmail.com>
2016-06-27 22:48:52 -0300

    Added modify_metamorphic.php

---
+++ public_html/modify_metamorphic.php
@@ -0,0 +1,87 @@
+<?php
+
+include '../resources/constants.php';
+header('Content-Type: text/html; charset=UTF-8');
+
+$valueName = $_GET['rock'];
+
+$user = Constants::getUser();
+$local = Constants::getLocal();
+$passwd = Constants::getPasswd();
+$database = Constants::getDatabase();
+$table = Constants::getTableMet();
+
+$name = Constants::getName();
+$main = Constants::getMainMetamorphic();
+$sec = Constants::getSecMetamorphic();
+$composition = Constants::getComposition();
+$structure = Constants::getStructure();
+$texture = Constants::getTexture();
+$metamorphism = Constants::getMetamorphism();
+$facies = Constants::getFacies();
+$protolith = Constants::getProtolith();
+$description = Constants::getDescription();
+
+$connection = mysql_connect($local, $user, $passwd) or die(mysql_error());
+mysql_select_db($database, $connection) or die(mysql_error());
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET character_set_connection=utf8");
+mysql_query("SET character_set_client=utf8");
+mysql_query("SET character_set_results=utf8");
+
+$sql = "SELECT * FROM $table WHERE $name='$valueName' ;";
+$result = mysql_query($sql, $connection) or die(mysql_error());
+$results = mysql_fetch_array($result);
+echo "<html>";
+echo "    <head>";
+echo '        <meta charset="utf-8">';
+echo "        <title>Modify Metamorphic Rock</title>";
+echo "    </head>";
+echo "    <body>";
+echo '        <form method="POST" action="">';
+echo "            <table>";
+echo "                <tr>";
+echo "                    <td> <label>Name</label></td>";
+echo '                    <td> <input type="text" name="field1" id="field1" value="'.$results[$name].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label>Main Mineralogy</label></td>";
+echo '                    <td> <textarea type="text" name="field2"id="field2" value="'.$results[$main].'"></textarea></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label>Secondary Mineralogy</label></td>";
+echo '                    <td> <textarea type="text" name="field3"id="field3" value="'.$results[$sec].'"></textarea></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label> Composition</label> </td>";
+echo '                    <td> <input type="text" name="field4"id="field4" value="'.$results[$composition].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label> Structure</label></td>";
+echo '                    <td> <input type="text" name="field5" id="field5" value="'.$results[$structure].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label>Texture</label></td>";
+echo '                    <td> <input type="text" name="filed6" id="field6" value="'.$results[$texture].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> <label>Metamorphism</label></td>";
+echo '                    <td> <input type="text" name="field7" id="field7" value="'.$results[$metamorphism].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> Facies</td>";
+echo '                    <td> <input type="text" name="field8" id="field8" value="'.$results[$facies].'" ></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> Protolith</td>";
+echo '                    <td> <input type="text" name="field9" id="field9" value="'.$results[$protolith].'"></td>';
+echo "                </tr>";
+echo "                <tr>";
+echo "                    <td> Description</td>";
+echo '                    <td> <textarea type="text" name="field10" id="field10"value="'.$results[$description].'"></textarea></td>';
+echo "                </tr>";
+echo "            </table>";
+echo "        </form>    ";
+echo "    </body>    ";
+echo "</html>";
+mysql_close($connection);
